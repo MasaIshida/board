02b94ca8621101ea6c9ad0017ed0d614a2bb951b
MasaIshida <53788864+MasaIshida@users.noreply.github.com>
2020-01-20 22:59:42 +0900

    Update settings.py

--- ForeignProject/settings.py
+++ ForeignProject/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'board_db',
-        'USER': 'root',
-        'PASSWORD': 'Kimoto0715%',
+        'USER': '######',
+        'PASSWORD': '########',
         'HOST': '',
         'PORT': '',
     }
